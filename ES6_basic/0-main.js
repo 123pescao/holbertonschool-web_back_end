91b87a6c0bf9ffa58aaf4fe022e7f1b396d8c87e
Joel <joel.caban2017@gmail.com>
2024-11-13 20:52:27 -0400

    Task 0

---
+++ ES6_basic/0-main.js
@@ -0,0 +1,5 @@
+// Import the functions from the module you created
+import { taskFirst, taskNext } from './0-constants.js';
+
+// Log the results to the console to see the output
+console.log(`${taskFirst()} ${taskNext()}`);
