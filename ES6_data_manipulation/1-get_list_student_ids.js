2abec2925dd0bacee8876ea477aa4ff54faa9c17
Joel <joel.caban2017@gmail.com>
2024-11-21 12:59:23 -0400

    Task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
-
-export default function getListStudentIds(student){
-    if(!Array.isArray(student)){
-        return [];
-    }
-    return student.map(item => item.id);
+export default function getListStudentIds(student) {
+  if (!Array.isArray(student)) {
+    return [];
+  }
+  return student.map((item) => item.id);
 }
