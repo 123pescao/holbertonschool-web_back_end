3ddf3a1c29039d74d799413e0d9bb6e53dbac39c
Joel <joel.caban2017@gmail.com>
2024-11-21 14:24:25 -0400

    Task 2

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+export default function getStudentsByLocation(student, city) {
+  if (!Array.isArray(student)) {
+    throw new Error('Invalid input: student must be an array');
+  }
+
+  if (typeof city !== 'string') {
+    throw new Error('Invalid input: city must be a string');
+  }
+
+  return student.filter((student) => student.location === city);
+}
