6acfe8331e3db2a0235022f4b2589e4473bcf928
Joel <joel.caban2017@gmail.com>
2024-11-21 13:50:16 -0400

    Task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,19 @@
 export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
 
-    if (!startString || typeof startString !== 'string') {
-        return '';
+  if (!(set instanceof Set)) {
+    return '';
+  }
+
+  const result = [];
+
+  for (const item of set) {
+    if (typeof item === 'string' && item.startsWith(startString)) {
+      result.push(item.slice(startString.length));
     }
+  }
 
-
-    if (!(set instanceof Set)) {
-        return '';
-    }
-
-    const result = [];
-
-    for (const item of set) {
-        if (typeof item === 'string' && item.startsWith(startString)) {
-            result.push(item.slice(startString.length));
-        }
-    }
-
-    return result.join('-');
+  return result.join('-');
 }
