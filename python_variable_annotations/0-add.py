a78e18aab2454b338e9a6a5223a895bdc6086ab6
Joel <joel.caban2017@gmail.com>
2024-12-03 12:05:15 -0400

    Task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""
+Module creates a type-annotated function add that
+takes a float a and a float b as arguments and
+returns their sum as a float.
+"""
 
 
-def add(a, b):
-    a = float(a)
-    b = float(b)
-    sum = float(a) + float(b)
-    return float(sum)
+def add(a: float, b: float) -> float:
+    """Adds 2 floats"""
+    return a + b
