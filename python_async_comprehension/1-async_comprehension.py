2ea8faee889e8d7169c54e0c487ffefc42a41ad1
Joel <joel.caban2017@gmail.com>
2024-12-10 17:58:34 -0400

    Task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-A coroutine called async_generator that takes no arguments.
-The coroutine will loop 10 times, each time asynchronously wait 1 second,
-then yield a random number between 0 and 10.
-Using the random module.
+Module creates a coroutine called async_generator
+that takes no arguments. The coroutine loops 10 times,
+each time asynchronously waiting 1 second,
+then yields a random number between 0 and 10.
 """
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
